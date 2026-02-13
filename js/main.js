fd4bab6d7ed2db5929348a7377ebb7fd3fd43295
seira21 <kafkasakira4@gmail.com>
2026-02-13 23:47:53 +0700

    Update title text in main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('THIS FLOWERS FOR YOU PRINCES,I LOVE U').split('')
+    const titles = ('FLOWERS FOR YOU PRINCES,I LOVE U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -19,4 +19,3 @@ onload = () => {
 
     clearTimeout(c);
   }, 1000);
-};
